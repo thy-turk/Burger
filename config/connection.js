70cad1fbbcb364ddd896fe290773a716ff37e025
Turk <tuckerfiumara@gmail.com>
2020-01-17 23:25:03 -0700

    Got connections functioning

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,4 @@
 var mysql = require("mysql");
-var express = require("express");
-var app = express();
-
-var PORT = process.env.PORT || 8080;
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -20,9 +16,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-app.listen(PORT, function() {
-    // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
-  });
 
 module.exports = connection;
