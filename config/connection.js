6393aaa9c61102b07be3eabf7d3654448312ba2b
Turk <tuckerfiumara@gmail.com>
2020-01-18 19:51:26 -0700

    Testing connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,20 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
   port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
+  user: "	sxrn262bva23340t",
+  password: "	je8hb0umbmy1kr4v",
+  database: "	jb3v1gm87wa3imka"
 });
+
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "password",
+//   database: "burgers_db"
+// });
 
 connection.connect(function(err) {
   if (err) {
