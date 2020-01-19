bd58e7e777e7dc4cde77c03b9b829cc5f97fb499
Turk <tuckerfiumara@gmail.com>
2020-01-18 19:18:43 -0700

    Updated Readme

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheese", false), ("Cheeseburger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Avacado Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Egg Burger", true);
