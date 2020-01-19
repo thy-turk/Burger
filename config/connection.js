3d6ebdb3b82121149f4639450fad42724e3acd9a
Turk <tuckerfiumara@gmail.com>
2020-01-18 20:17:04 -0700

    Working on JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,20 @@
 var mysql = require("mysql");
 
-connection = mysql.createConnection("mysql://sxrn262bva23340t:je8hb0umbmy1kr4v@tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jb3v1gm87wa3imka");
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
 
-// var connection = mysql.createConnection({
-//   host: "tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
-//   port: 3306,
-//   user: "	sxrn262bva23340t",
-//   password: "	je8hb0umbmy1kr4v",
-//   database: "	jb3v1gm87wa3imka"
-// });
 
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "password",
-//   database: "burgers_db"
-// });
+
 
 connection.connect(function(err) {
   if (err) {
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,6 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Cheese", false), ("Cheeseburger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES 
+("Bacon Cheese", false), 
+("Cheeseburger", true),
+("Avacado Burger", false),
+("Tasty Burger", true);
 
-INSERT INTO burgers (burger_name, devoured) VALUES ("Avacado Burger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Egg Burger", true);
